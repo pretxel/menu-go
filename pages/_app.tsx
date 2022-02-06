abf62a3a3dd213465173254999b2d878a60dfb9d
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-02-06 10:34:14 -0600

    fix: delete var to test husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  let app = ''
   return <Component {...pageProps} />
 }
 
