e5f321f9751787b9d2d3fd0eeab4dc3b7d46a8da
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-05 13:09:51 -0500

    🛠 page dishes

---
+++ pages/panel/dishes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VerticalLayout from '../../../components/Layout/VerticalLayout'
+
+type Props = {}
+
+export default function index({}: Props) {
+  return (
+    <VerticalLayout>
+      <h1>Estos son los dishes mano</h1>
+    </VerticalLayout>
+  )
+}
