a1a27db4d1fdff79e845c5f0fd38792246727ef9
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-05 13:13:40 -0500

    ✨ logout in the footer of panel

--- components/Sidebar/menu/footer.tsx
+++ components/Sidebar/menu/footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { useMemo } from 'react'
 import Link from 'next/link'
 import randomAvatar from '../../../lib/randomAvatar'
-import { Divider, Avatar, Row, Col } from 'antd'
-import { RiSettings3Line } from 'react-icons/ri'
+import { Avatar, Row, Col } from 'antd'
 import { Session } from 'next-auth'
+import DropDownFooter from './dropdownFooter'
 
 interface FooterProps {
   collapsed: Boolean
@@ -12,47 +12,9 @@ interface FooterProps {
 }
 
 export default function MenuFooter(props: FooterProps) {
-  const avatarImg = randomAvatar()
-  const { session } = props
+  const memoizedAvatar = useMemo(() => randomAvatar(), [])
   return props.collapsed === false ? (
-    <Row
-      className="hp-sidebar-footer hp-pb-24 hp-px-24 hp-bg-color-dark-100"
-      align="middle"
-      justify="space-between"
-    >
-      <Divider className="hp-border-color-black-20 hp-border-color-dark-70 hp-mt-0" />
-
-      <Col>
-        <Row align="middle">
-          <Avatar size={36} src={avatarImg} className="hp-mr-8" />
-
-          <div>
-            <span className="hp-d-block hp-text-color-black-100 hp-text-color-dark-0 hp-p1-body">
-              {session?.user?.name}
-            </span>
-
-            <Link href="/pages/profile/personel-information">
-              <a
-                className="hp-badge-text hp-text-color-dark-30"
-                onClick={props.onClose}
-              >
-                View Profile
-              </a>
-            </Link>
-          </div>
-        </Row>
-      </Col>
-
-      <Col>
-        <Link href="/pages/profile/security" passHref>
-          <RiSettings3Line
-            className="remix-icon hp-text-color-black-100 hp-text-color-dark-0"
-            size={24}
-            onClick={props.onClose}
-          />
-        </Link>
-      </Col>
-    </Row>
+    <DropDownFooter {...props} />
   ) : (
     <Row
       className="hp-sidebar-footer hp-pt-16 hp-mb-16 hp-bg-color-dark-100"
@@ -62,7 +24,7 @@ export default function MenuFooter(props: FooterProps) {
       <Col>
         <Link href="/pages/profile/personel-information">
           <a onClick={props.onClose}>
-            <Avatar size={36} src={avatarImg} />
+            <Avatar size={36} src={memoizedAvatar} />
           </a>
         </Link>
       </Col>
