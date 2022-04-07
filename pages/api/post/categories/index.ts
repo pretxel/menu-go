c6e1623cf34c73b15c7daf9edcab6a0c60033d00
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-07 12:57:01 -0500

    🛠 post categories api

--- pages/api/post/categories/index.ts
+++ pages/api/post/categories/index.ts
@@ -4,14 +4,13 @@ import prisma from '@lib/prisma'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // post
-  const { name, address, phone, image } = req.body
+  const { name, description } = req.body
+  console.log(req.body, 'body')
   try {
-    const data = await prisma.configRestaurant.create({
+    const data = await prisma.categories.create({
       data: {
         name,
-        address,
-        phone,
-        image,
+        description,
       },
     })
 
