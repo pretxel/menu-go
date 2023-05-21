bcabbdfa61e1348d5b2b2cdc8b686d7015ee24da
Edsel <pretxel@hotmail.com>
2023-05-21 19:57:10 +0100

    chore(core): update panel and login page

--- apps/menu-go/src/components/Panel/Layout/index.tsx
+++ apps/menu-go/src/components/Panel/Layout/index.tsx
@@ -1,9 +1,9 @@
-import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { signOut,useSession  } from 'next-auth/react';
+import { Fragment } from 'react';
+
 import { SkeletonCard } from '../../skeleton-card';
-import { useSession } from 'next-auth/react';
-import { signOut } from 'next-auth/react';
 
 const user = {
   name: 'Tom Cook',
