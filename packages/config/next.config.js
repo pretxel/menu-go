8e993f2d19b1b7951b14541444916e121a5e028c
Edsel Serrano <edselserrano@Edsels-MacBook-Pro-2.local>
2023-11-25 00:56:40 +0000

    chore: update deps

--- packages/config/next.config.js
+++ packages/config/next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
   // output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
-    appDir: true,
   },
   images: {
     remotePatterns: [
