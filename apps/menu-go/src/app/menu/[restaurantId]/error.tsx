28b30d14a6225d0354aac22c33f405a2a8796b51
Edsel Serrano <edselserrano@Edsels-MacBook-Pro-2.local>
2023-12-02 16:31:06 +0000

    fix: optim SEO and scripts

--- apps/menu-go/src/app/menu/[restaurantId]/error.tsx
+++ apps/menu-go/src/app/menu/[restaurantId]/error.tsx
@@ -1,12 +1,11 @@
 'use client';
+import Link from 'next/link';
 import { useEffect } from 'react';
 
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
@@ -24,12 +23,12 @@ export default function Error({
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <Link
             href="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
+          </Link>
         </div>
       </div>
     </main>
--- apps/menu-go/src/components/Uploader/index.tsx
+++ apps/menu-go/src/components/Uploader/index.tsx
@@ -58,52 +58,8 @@ export default function Uploader({ dishId }) {
             const { url } = (await res.json()) as BlobResult;
             await updateDish(dishId, url);
             toast.success('Image uploaded!');
-            // toast(
-            //   (t) => (
-            //     <div className="relative">
-            //       <div className="p-2">
-            //         <p className="font-semibold text-gray-900">
-            //           File uploaded!
-            //         </p>
-            //         <p className="mt-1 text-sm text-gray-500">
-            //           Your file has been uploaded to{' '}
-            //           <a
-            //             className="font-medium text-gray-900 underline"
-            //             href={url}
-            //             target="_blank"
-            //             rel="noopener noreferrer"
-            //           >
-            //             {url}
-            //           </a>
-            //         </p>
-            //       </div>
-            //       <button
-            //         onClick={() => toast.dismiss(t.id)}
-            //         className="absolute top-0 -right-2 inline-flex text-gray-400 focus:outline-none focus:text-gray-500 rounded-full p-1.5 hover:bg-gray-100 transition ease-in-out duration-150"
-            //       >
-            //         <svg
-            //           className="h-5 w-5"
-            //           viewBox="0 0 20 20"
-            //           fill="currentColor"
-            //         >
-            //           <path
-            //             fillRule="evenodd"
-            //             d="M5.293 5.293a1 1 0 011.414 0L10
-            //               8.586l3.293-3.293a1 1 0 111.414 1.414L11.414
-            //               10l3.293 3.293a1 1 0 01-1.414 1.414L10
-            //               11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586
-            //               10 5.293 6.707a1 1 0 010-1.414z"
-            //             clipRule="evenodd"
-            //           />
-            //         </svg>
-            //       </button>
-            //     </div>
-            //   ),
-            //   { duration: 300000 }
-            // );
           } else {
-            const error = await res.text();
-            // toast.error(error);
+            toast.error("Image couldn't be uploaded!");
           }
           setSaving(false);
         });
