a41741cbeade3c405fd2ae5848bb648270c721c5
Abraham Serrano <abrahamserrano96@hotmail.com>
2024-01-02 12:39:56 -0600

    feat: small styles and ux

--- apps/menu-go/src/app/panel/layout.tsx
+++ apps/menu-go/src/app/panel/layout.tsx
@@ -33,7 +33,7 @@ export default function Layout({ title, children }) {
               </div>
             </header>
             <main>
-              <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 {children}
               </div>
             </main>
